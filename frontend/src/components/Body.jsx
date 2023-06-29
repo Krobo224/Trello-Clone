8a05fde5665873a013b57a62de02176ac0c80cd8
krobo <shirodekaushal@gmail.com>
2023-06-29 13:11:14 +0530

    dnd-4

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,70 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 // import Filter from "./Filter";
 import List from "./List";
-import { DragDropContext } from "react-beautiful-dnd";
 
-const reorderList = (sourceList, startIndex, endIndex) => {
-  const newTaskIds = Array.from(sourceList.id);
-  const [removed] = newTaskIds.splice(startIndex, 1); // removing
-  newTaskIds.splice(endIndex, 0, removed); // not removing any item
 
-  const newList = {
-    ...sourceList,
-    taskIds: newTaskIds,
-  }
-
-  return newList;
-}
 
 const Body = () => {
-  const [state, setState] = useState(false);
-  const onDragEnd = (result) => {
-    const { destination, source } = result;
-
-  //   // If user tries to drop in unknown destination
-  //   if (!destination) return;
-
-  //   // If user destination == source
-  //   if (destination.droppableId === source.droppableId && destination.index === source.index) {
-  //     return;
-  //   }
-
-  //   // If the user drops within same list but in different postion
-  //   const sourceList = state.list[source.droppableId];
-  //   const destinationList = state.list[destination.droppableId]
-
-  //   if(sourceList.id === destinationList.id) {
-  //     const newList = reorderList(
-  //       sourceList,
-  //       source.index,
-  //       destination.index
-  //     )
-
-  //     const newState = {
-  //       ...state, 
-  //       list: {
-  //         ...state.list,
-  //         [newList.id]: newList
-  //       },
-  //     };
-  //     setState(newState);
-  //     return;
-  //   }
-
-    // If user drops to other list
-
-
-  };
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div className="p-3 w-full border">
-        {/* <Filter /> */}
-        <div className="flex flex-wrap ">
-          <List />
-        </div>
+    <div className="p-3 w-full border">
+      {/* <Filter /> */}
+      <div className="flex flex-wrap ">
+        <List />
       </div>
-    </DragDropContext>
+    </div>
   );
 };
 
--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -2,14 +2,63 @@ import React from "react";
 import Card from "./Card";
 import AddNew from "./AddNew";
 import { useSelector } from "react-redux";
-import { useDrag, useDrop } from "react-dnd";
 import { Draggable, Droppable } from "react-beautiful-dnd";
+import { useState, useEffect } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
 
 const List = () => {
   const listItem = useSelector((store) => store.listSlice.list);
+  const [state, setState] = useState(listItem);
+  
+  // console.log("state ", state);
+  // console.log("LIST ITEM ", listItem);
+
+  const onDragEnd = (result) => {
+    console.log("result: ", result);
+    const { destination, source } = result;
+
+    // If user tries to drop in unknown destination
+    if (!destination) return;
+
+    // If user destination === source
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // Retrieve the source and destination lists from the state
+    const sourceListIndex = state.listSlice.list.findIndex(
+      (item) => item.id === source.droppableId
+    );
+    const destinationListIndex = state.listSlice.list.findIndex(
+      (item) => item.id === destination.droppableId
+    );
+    // console.log("----------", list);
+    const updatedListItem = { ...listItem };
+
+    // Reorder cards within the same list
+    if (source.droppableId === destination.droppableId) {
+      const sourceList = updatedListItem.listSlice.list[sourceListIndex];
+      const [draggedItem] = sourceList.children.splice(source.index, 1);
+      sourceList.children.splice(destination.index, 0, draggedItem);
+    } else {
+
+      // Move card from source list to destination list
+      const sourceList = updatedListItem.listSlice.list[sourceListIndex];
+      const destinationList =
+        updatedListItem.listSlice.list[destinationListIndex];
+
+      const [draggedItem] = sourceList.children.splice(source.index, 1);
+      destinationList.children.splice(destination.index, 0, draggedItem);
+    }
+
+    setState(updatedListItem);
+  };
 
   return (
-    <>
+    <DragDropContext onDragEnd={onDragEnd}>
       {listItem.map((list) => (
         <div className="p-3 w-1/3" key={list.id}>
           <div className="p-3 bg-gray-200">
@@ -20,7 +69,7 @@ const List = () => {
                   ref={droppableProvided.innerRef}
                   {...droppableProvided.droppableProps}
                 >
-                  {list?.children?.length > 0 &&
+                  {list.children && list.children.length > 0 ? (
                     list.children.map((children, index) => (
                       <Draggable
                         key={children.id}
@@ -38,7 +87,11 @@ const List = () => {
                           </div>
                         )}
                       </Draggable>
-                    ))}
+                    ))
+                  ) : (
+                    <div>No cards</div>
+                  )}
+                  {droppableProvided.placeholder}
                   <div className="mt-3">
                     <AddNew type="card" parentId={list.id} />
                   </div>
@@ -54,7 +107,7 @@ const List = () => {
           <AddNew />
         </div>
       </div>
-    </>
+    </DragDropContext>
   );
 };
 
